ee3533cd1baf9f30e2f1962440f39a659cba07a4
Mehmet Akgun <mehmetakgun17062001@gmail.com>
2024-08-22 10:53:48 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Mehmet";
+            int yas = 23;
+            kurs kurs1 = new kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.egitmen = "Mehmet Akgun";
+            kurs1.izlenmeOrani = 68;
+
+            kurs kurs2 = new kurs();
+            kurs2.kursAdi = "Java";
+            kurs2.egitmen = "Umut Ayan";
+            kurs2.izlenmeOrani = 80;
+
+
+
+            kurs[] kurslar = new kurs[] { kurs1, kurs2 };
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.kursAdi + " : " + kurs.egitmen);
+
+            }
+        }
+    }
+    class kurs
+    {
+        public string kursAdi { get; set; }
+        public string egitmen { get; set; }
+        public int izlenmeOrani { get; set; }
+
+
+    }
+}
