6a57ead6fdbc8810bc434832e7fc79066a1eb053
Mehmet Akgun <mehmetakgun17062001@gmail.com>
2024-08-23 13:14:33 +0300

    inheritance, interface, method injection

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    // Business Classes
+
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mehmet Akgün
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Mehmet";
+            musteri1.Soyadi = "Akgün";
+            musteri1.MusteriNo = "12345";
+            musteri1.TcNo = "12345678910";
+
+
+            // Kodlama.io Şirket
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+
+        }
+    }
+
+}
