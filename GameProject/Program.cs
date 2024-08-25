f4d01331eed98addbc7b35848a1099aa36e51067
Mehmet Akgun <mehmetakgun17062001@gmail.com>
2024-08-25 16:16:42 +0300

    C# oyun alıştırma

---
+++ GameProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer 
+            {
+                Id = 1,
+                BirthYear = 2001,
+                FirstName = "Mehmet",
+                LastName = "AKGUN",
+                IdentityNumber = 12345 
+            });
+
+        }
+    }
+}
